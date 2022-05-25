ef681b02fe7672cb667c50e95bcd7f7ab908cb4d
mo3tasem <moatasemmohamed39@gmail.com>
2022-05-25 22:39:04 +0200

    Initial commit

---
+++ lib/global/Icons/icons.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:flutter/material.dart';
+
+class MyIcons {
+  MyIcons._();
+
+
+  static const _kFontFam = 'foodhublogo';
+  static const IconData logo = IconData(0xe800, fontFamily: _kFontFam);
+
+
+  static const _appname = 'Appname';
+  static const IconData appname = IconData(0xe800, fontFamily: _appname,);
+
+  static const _fulllogo = 'fulllogo';
+
+
+  static const IconData fulllogo = IconData(0xe800, fontFamily: _fulllogo);
+
+
+}
